ab8e0c53f879dd138b91690b62e25cc7b6011c12
Kwame Carr <kwame_91@hotmail.com>
2023-03-19 21:25:25 +0100

    init

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Bimssolutions</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Op maat gemaakte weboplossingen voor jou" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
